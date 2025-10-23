c5bf98fb29fa3ba42a70ee81bc528151db27e506
Chaoshuo Han <ucfnch0@ucl.ac.uk>
2025-10-24 04:04:08 +0800

    Second Commit

--- mkr1010_mqtt_simple.ino
+++ mkr1010_mqtt_simple.ino
@@ -5,8 +5,8 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <PubSubClient.h>
-#include "arduino_secrets.h" 
-#include <utility/wifi_drv.h>   // library to drive to RGB LED on the MKR1010
+#include "arduino_secrets.h"
+#include <utility/wifi_drv.h>  // library to drive to RGB LED on the MKR1010
 
 /*
 **** please enter your sensitive data in the Secret tab/arduino_secrets.h
@@ -16,34 +16,37 @@
 #define SECRET_MQTTUSER "user name - eg student"
 #define SECRET_MQTTPASS "password";
  */
-const char* ssid          = SECRET_SSID;
-const char* password      = SECRET_PASS;
-const char* ssid1         = SECRET_SSID1;
-const char* password1     = SECRET_PASS1;
+const char* ssid = SECRET_SSID;
+const char* password = SECRET_PASS;
 const char* mqtt_username = SECRET_MQTTUSER;
 const char* mqtt_password = SECRET_MQTTPASS;
-const char* mqtt_server   = "mqtt.cetools.org";
-const int mqtt_port       = 1884;
+const char* mqtt_server = "mqtt.cetools.org";
+const int mqtt_port = 1884;
 
 // create wifi object and mqtt object
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
 // Make sure to update your lightid value below with the one you have been allocated
-String lightId = "1"; // the topic id number or user number being used.
+String lightId = "10";  // the topic id number or user number being used.
 
 // Here we define the MQTT topic we will be publishing data to
-String mqtt_topic = "student/CASA0014/luminaire/" + lightId;            
-String clientId = ""; // will set once i have mac address so that it is unique
+String mqtt_topic = "student/CASA0014/luminaire/" + lightId;
+String clientId = "";  // will set once i have mac address so that it is unique
 
-// NeoPixel Configuration - we need to know this to know how to send messages 
-// to vespera 
+// NeoPixel Configuration - we need to know this to know how to send messages
+// to vespera
 const int num_leds = 72;
-const int payload_size = num_leds * 3; // x3 for RGB
+const int payload_size = num_leds * 3;  // x3 for RGB
 
-// Create the byte array to send in MQTT payload this stores all the colours 
+// Create the byte array to send in MQTT payload this stores all the colours
 // in memory so that they can be accessed in for example the rainbow function
 byte RGBpayload[payload_size];
+
+// --- FSR sensor setup ---
+#define FSR_PIN A0  // sensor pin
+int fsrValue = 0;   // store raw data
+
 
 void setup() {
   Serial.begin(115200);
@@ -67,29 +70,37 @@ void setup() {
   mqttClient.setServer(mqtt_server, mqtt_port);
   mqttClient.setBufferSize(2000);
   mqttClient.setCallback(callback);
-  
+
+  pinMode(FSR_PIN, INPUT);           // setup FSR pin
+  Serial.println("FSR test ready");  // check serial output
+
+
   Serial.println("Set-up complete");
 }
- 
+
 void loop() {
   // Reconnect if necessary
   if (!mqttClient.connected()) {
     reconnectMQTT();
   }
-  
-  if (WiFi.status() != WL_CONNECTED){
+
+  if (WiFi.status() != WL_CONNECTED) {
     startWifi();
   }
   // keep mqtt alive
   mqttClient.loop();
 
+  // --- FSR test ---
+  fsrValue = analogRead(FSR_PIN);
+  Serial.print("FSR value: "+ fsrValue);  
+  delay(200); 
 
 
 
-  for(int n=0; n<num_leds; n++){
+  for (int n = 0; n < num_leds; n++) {
     send_all_off();
     delay(100);
-    send_RGB_to_pixel(0,250,0,n);
+    send_RGB_to_pixel(0, 250, 0, n);
     delay(200);
   }
 
@@ -102,13 +113,13 @@ void send_RGB_to_pixel(int r, int g, int b, int pixel) {
   // Check if the mqttClient is connected before publishing
   if (mqttClient.connected()) {
     // Update the byte array with the specified RGB color pattern
-    RGBpayload[pixel * 3 + 0] = (byte)r; // Red
-    RGBpayload[pixel * 3 + 1] = (byte)g; // Green
-    RGBpayload[pixel * 3 + 2] = (byte)b; // Blue
+    RGBpayload[pixel * 3 + 0] = (byte)r;  // Red
+    RGBpayload[pixel * 3 + 1] = (byte)g;  // Green
+    RGBpayload[pixel * 3 + 2] = (byte)b;  // Blue
 
     // Publish the byte array
     mqttClient.publish(mqtt_topic.c_str(), RGBpayload, payload_size);
-    
+
     Serial.println("Published whole byte array after updating a single pixel.");
   } else {
     Serial.println("MQTT mqttClient not connected, cannot publish from *send_RGB_to_pixel*.");
@@ -119,14 +130,14 @@ void send_all_off() {
   // Check if the mqttClient is connected before publishing
   if (mqttClient.connected()) {
     // Fill the byte array with the specified RGB color pattern
-    for(int pixel=0; pixel < num_leds; pixel++){
-      RGBpayload[pixel * 3 + 0] = (byte)0; // Red
-      RGBpayload[pixel * 3 + 1] = (byte)0; // Green
-      RGBpayload[pixel * 3 + 2] = (byte)0; // Blue
+    for (int pixel = 0; pixel < num_leds; pixel++) {
+      RGBpayload[pixel * 3 + 0] = (byte)0;  // Red
+      RGBpayload[pixel * 3 + 1] = (byte)0;  // Green
+      RGBpayload[pixel * 3 + 2] = (byte)0;  // Blue
     }
     // Publish the byte array
     mqttClient.publish(mqtt_topic.c_str(), RGBpayload, payload_size);
-    
+
     Serial.println("Published an all zero (off) byte array.");
   } else {
     Serial.println("MQTT mqttClient not connected, cannot publish from *send_all_off*.");
@@ -137,14 +148,14 @@ void send_all_random() {
   // Check if the mqttClient is connected before publishing
   if (mqttClient.connected()) {
     // Fill the byte array with the specified RGB color pattern
-    for(int pixel=0; pixel < num_leds; pixel++){
-      RGBpayload[pixel * 3 + 0] = (byte)random(50,256); // Red - 256 is exclusive, so it goes up to 255
-      RGBpayload[pixel * 3 + 1] = (byte)random(50,256); // Green
-      RGBpayload[pixel * 3 + 2] = (byte)random(50,256); // Blue
+    for (int pixel = 0; pixel < num_leds; pixel++) {
+      RGBpayload[pixel * 3 + 0] = (byte)random(50, 256);  // Red - 256 is exclusive, so it goes up to 255
+      RGBpayload[pixel * 3 + 1] = (byte)random(50, 256);  // Green
+      RGBpayload[pixel * 3 + 2] = (byte)random(50, 256);  // Blue
     }
     // Publish the byte array
     mqttClient.publish(mqtt_topic.c_str(), RGBpayload, payload_size);
-    
+
     Serial.println("Published an all random byte array.");
   } else {
     Serial.println("MQTT mqttClient not connected, cannot publish from *send_all_random*.");
